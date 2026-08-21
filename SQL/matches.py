288738bd256f6a103a2f4445e1bbc0fe7406b631
Alix Menard <amenard@ec-m.fr>
2026-08-21 12:41:09 +0200

    recreate table after delete...

--- SQL/matches.py
+++ SQL/matches.py
@@ -1,8 +1,10 @@
 from SQL.tables import delete_table
+from database.creation.trades import init_matches
 import sqlite3
 
 def update_matches(matches: list):
     delete_table("matches")
+    init_matches()
 
     conn = sqlite3.connect("database/app.db")
     cursor = conn.cursor()
