7af252f5402180a9c0074949765e1c315caee8f0
Alix Menard <amenard@ec-m.fr>
2026-08-21 12:55:11 +0200

    debug

--- SQL/matches.py
+++ SQL/matches.py
@@ -11,7 +11,8 @@ def update_matches(matches: list):
 
     for match in matches:
         card_id, giver_id, receiver_id, quantity, money, trade = match
-        money, trade = bool(money), bool(trade)
+        for el in match:
+            print(type(el), el)
         cursor.executemany(
             """INSERT INTO matches (card_id, giver_id, receiver_id, quantity, money, trade)
             VALUES (?, ?, ?, ?, ?, ?)""",
